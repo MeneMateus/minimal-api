2b830e13227e84d0014c58cb735f028af394db56
MeneMateus <mateusboxxx@gmail.com>
2024-08-31 16:54:31 -0300

    feat: create LoginDTOs and Login Controller

---
+++ Infra/db/DbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MinimalApi.Infra.Db;
+
+class DbContext : DbContext
+{
+    
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+using MinimalApi.DTOs;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if(loginDTO.Email == "adm@teste.com" && loginDTO.Password == "root"){
+        return Results.Ok();
+    }else {
+        return Results.Unauthorized();
+    }
+    });
+
+app.Run();
+
